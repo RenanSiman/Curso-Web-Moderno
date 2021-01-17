3f1fef438df1b327e0dba3a438bb834ef7e1e7c9
Renan Siman <31517127+RenanSiman@users.noreply.github.com>
2021-01-16 21:07:40 -0300

    add until 195

---
+++ exercicios-funcao/q01-basicOperations.js
@@ -0,0 +1,22 @@
+// função padrão
+function soma(a,b){
+    return a+b
+}
+console.log(soma(2,3))
+
+function subtracao(a,b){
+    return a-b
+}
+console.log(subtracao(2,3))
+
+function multiplicacao(a,b){
+    return a*b
+}
+console.log(multiplicacao(2,3))
+
+function divisao(a,b){
+    return a/b
+}
+console.log(divisao(2,3))
+
+// arrow function
